2af53adf10f7e594c77ccc48315e9c8a2a5f98ca
Deathviper039 <131456206+Deathviper039@users.noreply.github.com>
2023-08-03 13:00:17 +0530

    route

--- my-travel-app/src/App.js
+++ my-travel-app/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import About from './routes/About';
 import Home from './routes/Home';
-import {Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Service from './routes/Service';
 import Contact from './routes/Contact';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
+      <Router>
+      <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/service' element={<Service/>}/>
         <Route path='/contact' element={<Contact/>}/>
-      </Routes> */}
+      </Routes>
       <Navbar/>
-      
+      </Router>
       
     </div>
   );
